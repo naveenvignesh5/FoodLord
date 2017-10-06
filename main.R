7612d48a385e01e86add497ddc62e941de041658
Naveen Vignesh.B <naveenvignesh5.nv@gmail.com>
2017-10-07 02:42:20 +0530

    sorted out frequent tweet bugs

--- main.R
+++ main.R
@@ -1,3 +1,4 @@
+#------------------------- IMPORTING LIBRARIES ---------------------------
 library(wordcloud)
 library(twitteR)
 library(ROAuth)
@@ -7,8 +8,9 @@ library(RColorBrewer)
 library(topicmodels)
 library(ggplot2)
 library(sentiment)
+library(igraph)
 
-#Twitter Credentials
+#---------------------------TWITTER CREDENTIALS----------------------
 apiKey <- 'OY9AlUG1fM49rytP7T8hqInoL'
 apiSecret <- 'cjKBprKRaZoUUvwPUv7MX3NeRCehn8dSs9vBOwGjsbvAXBjI9s'
 access_token <- '2195494530-V7csBiOqiffi69fZuhkZEqMRZO5xqHHWcSiBl9V'
@@ -17,14 +19,16 @@ access_token_secret <- 'CQ96GaCgeIFqq2XVPtigJnFVD9momtZqFBEsHpH8n7q3Z'
 #setting up twitter third party authentication
 setup_twitter_oauth(apiKey,apiSecret,access_token,access_token_secret)
 
-#twitter search parameters
-searchString <- c('#indianfood')
-no <- 200
+#------------------------TWITTER SEARCH PARAMETERS---------------------
+searchString <- c("#indianfood")
+no <- 2000
 lang <- "en"
-tweets <- searchTwitter(searchString,no,lang,since = "2017-10-05",until = "2017-10-07")
+tweets <- searchTwitter(searchString,no,lang,since = "2017-10-01",until = "2017-10-07")
 
 tweets.df <- twListToDF(tweets)
 myCorpus <- Corpus(VectorSource(tweets.df$text)) #corpus for documents
+
+#----------------------- CLEANING THE TWEETS -------------------------
 
 myCorpus <- tm_map(myCorpus,removePunctuation)
 myCorpus <- tm_map(myCorpus,content_transformer(tolower))
@@ -33,7 +37,8 @@ myCorpus <- tm_map(myCorpus, removeWords, c("like", "video"))
 myCorpus <- tm_map(myCorpus, stripWhitespace)
 myCorpus <- tm_map(myCorpus, stemDocument)
 
-# Word cloud part
+# ---------------------- WORD CLOUD GENERATION OF FREQUENT TWEETS----------------
+
 tdm <- TermDocumentMatrix(myCorpus)
 m <- as.matrix(tdm)
 v <- sort(rowSums(m),decreasing=TRUE)
@@ -43,7 +48,7 @@ set.seed(1234)
 pal <- brewer.pal(9,"BuGn")[-(1:4)]
 wordcloud(words = d$word, freq = d$freq, min.freq = 5,max.words=200, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
 
-#segration of tweets based on a twitter code
+#-----------------------SEGREGATION OF TWEETS --------------------
 
 f = list()
 
@@ -78,28 +83,43 @@ topics <- data.frame(date=as.Date(tweets.df$created), topic=topics)
 
 ggplot(topics, aes(date, fill = term[topic])) +geom_density(position = "stack")
 
-#code to plot sentiment
+#------------------SENTIMENT ANALYSIS----------------------
 sentiments <- sentiment(foodCorpus$content)
 print(foodCorpus$content)
 
 table(sentiments$polarity)
 
-
-#create sentiments of scores
 sentiments$score <- 0
 sentiments$score[sentiments$polarity == "positive"] <- 1
 sentiments$score[sentiments$polarity == "negative"] <- -1
 #result <- aggregate(score ~ date, data = sentiments, sum)
 
 
-# select top retweeted tweets
-selected <- which(tweets.df$retweetCount >= 9)
+# ----------------MOST RETWEETED TWEETS--------------------
+selected <- which(tweets.df$retweetCount >= 15)
 # plot them
 dates <- strptime(tweets.df$created, format="%Y-%m-%d")
-plot(x=dates, y=tweets.df$retweetCount, type="b", col="grey",
+plot(x=dates, y=tweets.df$retweetCount, type="l", col="grey",
      xlab="Date", ylab="Times retweeted")
 colors <- rainbow(10)[1:length(selected)]
 points(dates[selected], tweets.df$retweetCount[selected],
        pch=19, col=colors)
 text(dates[selected], tweets.df$retweetCount[selected],
      tweets.df$text[selected], col=colors, cex=.9)
+
+# ---------------------- ASSOCIATION OF TWEETS ------------------------
+tdm <- tdm[names(tail(sort(rowSums(as.matrix(tdm))), 50)) ]
+
+m <- as.matrix(tdm)
+m[m>=1] <- 1
+m <- m %*% t(m)
+
+g <- graph.adjacency(m,weighted = T,mode="undirected")
+g <- simplify(g)
+
+V(g)$label <- V(g)$name
+V(g)$degree <- degree(g)
+
+set.seed(3952)
+network <- layout.fruchterman.reingold(g)
+plot(g,layout=network)
