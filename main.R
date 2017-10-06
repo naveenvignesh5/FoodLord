993718ba459f48fb76a836d787d197344427c48d
Naveen Vignesh.B <naveenvignesh5.nv@gmail.com>
2017-10-06 18:39:06 +0530

    added date parameter

--- main.R
+++ main.R
@@ -22,7 +22,7 @@ searchString <- c('#indianfood')
 no <- 100
 lang <- "en"
 
-tweets <- searchTwitter(searchString,no,lang)
+tweets <- searchTwitter(searchString,no,lang,since = "2017-10-04",until = "2017-10-06")
 tweets.df <- twListToDF(tweets)
 myCorpus <- Corpus(VectorSource(tweets.df$text)) #corpus for documents
 
@@ -43,6 +43,16 @@ set.seed(1234)
 pal <- brewer.pal(9,"BuGn")[-(1:4)]
 wordcloud(words = d$word, freq = d$freq, min.freq = 5,max.words=200, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
 
+#segration of tweets based on a twitter code
+
+#loop through each document
+for(i in 1:length(myCorpus)) {
+  #condition to check if document matches content
+  if(pmatch(toString(myCorpus[[i]]$content),c("chicken")) > 0) {
+    print(c("matches",i))
+  }
+}
+
 #topic modelling terms part
 dtm <- as.DocumentTermMatrix(tdm)
 lda <- LDA(dtm,k=8)
@@ -62,5 +72,4 @@ sentiments$score[sentiments$polarity == "positive"] <- 1
 sentiments$score[sentiments$polarity == "negative"] <- -1
 sentiments$date <- as.Date(tweets.df$created)
 result <- aggregate(score ~ date, data = sentiments, sum)
-print(result)
 
